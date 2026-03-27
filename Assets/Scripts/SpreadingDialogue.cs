b69735738ef11d0d6eba2cb07219b2c801fb6309
cosums <114266944+cosums@users.noreply.github.com>
2026-03-27 14:50:28 -0500

    Update SpreadingDialogue.cs

--- Assets/Scripts/SpreadingDialogue.cs
+++ Assets/Scripts/SpreadingDialogue.cs
@@ -9,9 +9,14 @@ public class SpreadingDialogue : MonoBehaviour
     [SerializeField] private DiffusionManager diffusionManager;
     [SerializeField] private TextMeshProUGUI timeText;
 
+    public void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
     public void UpdatePosition(RoomController room)
     {
         //transform.position = room.transform.position + positionOffest;
-        //timeText.text = $"{room.name}, {diffusionManager.time + 1}PM";
+        timeText.text = $"{room.name}, {diffusionManager.time + 1}PM";
     }
 }
