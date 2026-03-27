0cba32d5ba9cbeb089dfcca93b06ac4109362125
cosums <114266944+cosums@users.noreply.github.com>
2026-03-27 14:21:07 -0500

    tab indicators

--- Assets/Scripts/TabSwitcher.cs
+++ Assets/Scripts/TabSwitcher.cs
@@ -1,10 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TabSwitcher : MonoBehaviour
 {
     [SerializeField] private GameObject[] tabs;
+    [SerializeField] private Button[] buttons;
+    [SerializeField] private int startTab = 1;
+
+    private void Start()
+    {
+        SwitchTo(startTab);
+    }
 
     public void SwitchTo(int t)
     {
@@ -12,10 +20,12 @@ public class TabSwitcher : MonoBehaviour
         {
             if (t == i) 
             { 
-                tabs[i].SetActive(true); 
+                tabs[i].SetActive(true);
+                buttons[i].interactable = false;
             } else
             {
                 tabs[i].SetActive(false);
+                buttons[i].interactable = true;
             }
         }
     }
