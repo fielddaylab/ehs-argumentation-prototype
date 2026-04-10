81ca404f0111a56b51b9993b42118fe8ac31aac4
cosums <114266944+cosums@users.noreply.github.com>
2026-04-10 13:47:14 -0500

    UI for pollutant selection

---
+++ Assets/ImageCycler.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageCycler : MonoBehaviour
+{
+    [SerializeField] private Texture checkMark;
+    [SerializeField] private Texture xMark;
+
+    private RawImage rawImage;
+    private int cycleState = 0;
+    
+    void Start()
+    {
+        rawImage = GetComponent<RawImage>();
+        rawImage.enabled = false;
+    }
+
+    public void IncrementCycle()
+    {
+        cycleState = (cycleState + 1) % 3;
+        switch (cycleState)
+        {
+            case 0:
+                rawImage.enabled = false;
+                rawImage.texture = null;
+                break;
+            case 1:
+                rawImage.enabled = true;
+                rawImage.texture = checkMark;
+                break;
+            case 2:
+                rawImage.texture = xMark;
+                break;
+        }
+
+    }
+}
