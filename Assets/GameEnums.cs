9e416e56c23d2f995a61e93d5f915de6a17c517f
cosums <114266944+cosums@users.noreply.github.com>
2026-04-13 11:30:46 -0500

    timeline scriptable

--- Assets/GameEnums.cs
+++ Assets/GameEnums.cs
@@ -14,6 +14,12 @@ public enum PollutionSource
     Spraycan
 }
 
+public enum SourceAction
+{
+    On,
+    Off
+}
+
 public enum Pollutant
 {
     CO2,
@@ -24,6 +30,7 @@ public enum Pollutant
 
 public enum Symptom
 {
+    None,
     ShortBreath,
     LungIrritation,
     Headache,
@@ -35,4 +42,11 @@ public enum Symptom
     Nausea,
     Cough,
     Z
+}
+
+public enum CharacterType
+{
+    Roundy,
+    Blockhead,
+    Triangelo
 }
--- Assets/TimelineObject.cs
+++ Assets/TimelineObject.cs
@@ -2,8 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "ScriptableObjects/Pollution Level")]
+[CreateAssetMenu(menuName = "ScriptableObjects/Timeline Object")]
 public class TimelineObject : ScriptableObject
 {
-    
+    public TimelineStep[] timeline;
+}
+
+[System.Serializable]
+public class TimelineStep
+{
+    public int hourTime;
+    public RoomStep[] roomSteps;
+}
+
+[System.Serializable]
+public class RoomStep
+{
+    public RoomType roomType;
+    public PollutantStep[] pollutantSteps;
+    public CharacterStep[] characterSteps;
+}
+
+[System.Serializable]
+public class ObjectStep
+{
+    public PollutionSource pollutionSource;
+    public SourceAction sourceAction;
+}
+
+[System.Serializable]
+public class PollutantStep
+{
+    public Pollutant pollutantType;
+    public int concentration;
+}
+
+[System.Serializable]
+public class CharacterStep
+{
+    public CharacterType character;
+    public string dialogue;
+    public Symptom observedSymptom;
 }
